5e4081fdb92699a9a6b88433cd127d1c48afe720
Nishant Shah <nishant.shah@wwindia.com>
2019-10-31 15:55:57 +0530

    Jade

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 const express = require('express')
-const app = express()
-const comment = require('./data/entites/comment')
+var createError = require('http-errors');
+var path = require('path');
 
-app.get('/',(req,res)=> res.send(comment.one()));
+
+var indexRouter = require('./presentation/routes/index');
+var usersRouter = require('./presentation/routes/users');
+
+const app = express()
+
+// view engine setup
+app.set('views', path.join(__dirname, './presentation/views'));
+app.set('view engine', 'jade');
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.get('/heartbeat',(req,res) => res.send("Everything is working fine!"));
 
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(3000,()=>{
   console.log("REST API Server running");
---
+++ presentation/routes/users.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+const comment = require('../../data/entites/comment')
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource'+comment.one());
+});
+
+module.exports = router;
