bf30bbcdaf492689c66f7459545c1e9a2bdd5ea7
Nishant Shah <nishant.shah@wwindia.com>
2019-11-04 21:19:06 +0530

    Mongo working

--- data/entites/comment.js
+++ data/entites/comment.js
@@ -4,9 +4,40 @@ function Comment(){
     
 }
 
+const mongo = require('mongodb');
+const url = "mongodb://mongo:27017";
+
+mongo.connect(url, {useNewUrlParser: true}, (err, db) => {
+    if(err) {
+       console.log(err);
+       process.exit(0);
+    }
+
+    let data = [{
+      "id": 100,
+       "name": "Shahid"
+   },{
+       "id": 101,
+       "name": "Rahil"
+   },{
+       "id": 102,
+       "name": "John"
+   }];
+    var dbo = db.db('demo');
+    console.log('database connected!');
+    var collection = dbo.collection('users');
+    collection.insertMany(data, (err, result) => {
+        if(err) {
+            console.log(err);
+            process.exit(0);
+        }
+        console.log(result);
+        db.close();
+    });
+});
+
 module.exports = {
     one : function(){
-       logger.dblog().info("What is this");
         return "Hello users";
     },
 
